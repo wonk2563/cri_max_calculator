43ecdb34038ec53510abf49ac16e6e99c64592f3
wonk2563 <Tasumi1979@hotmail.com>
2021-03-20 21:12:39 +0800

    1.1.0

--- script/slider.js
+++ script/slider.js
@@ -6,6 +6,15 @@ function Change_MAXvalue(){
 	document.getElementById("originMAX").value = parseFloat(document.getElementById("MAX_slider").value);
 }
 
+function Change_CRIslidervalue(){
+	document.getElementById("CRI_slider").value = parseFloat(document.getElementById("originCRI").value);
+}
+
+function Change_MAXslidervalue(){
+	document.getElementById("MAX_slider").value = parseFloat(document.getElementById("originMAX").value);
+}
+
+
 
 function Add_CRIslider(){
 	document.getElementById("CRI_slider").value = parseFloat(document.getElementById("CRI_slider").value) + 0.1;
