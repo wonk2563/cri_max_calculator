04947f544b5d66fae55d1eb511ad25a8e427074e
wonk2563 <Tasumi1979@hotmail.com>
2021-03-20 20:59:47 +0800

    1.1.0
    新增微調滑桿與按鈕

---
+++ script/slider.js
@@ -0,0 +1,30 @@
+function Change_CRIvalue(){
+	document.getElementById("originCRI").value = parseFloat(document.getElementById("CRI_slider").value);
+}
+
+function Change_MAXvalue(){
+	document.getElementById("originMAX").value = parseFloat(document.getElementById("MAX_slider").value);
+}
+
+
+function Add_CRIslider(){
+	document.getElementById("CRI_slider").value = parseFloat(document.getElementById("CRI_slider").value) + 0.1;
+	document.getElementById("originCRI").value = parseFloat(document.getElementById("CRI_slider").value);
+}
+
+function Dec_CRIslider(){
+	document.getElementById("CRI_slider").value = parseFloat(document.getElementById("CRI_slider").value) - 0.1;
+	document.getElementById("originCRI").value = parseFloat(document.getElementById("CRI_slider").value);
+}
+
+
+
+function Add_MAXslider(){
+	document.getElementById("MAX_slider").value = parseFloat(document.getElementById("MAX_slider").value) + 0.1;
+	document.getElementById("originMAX").value = parseFloat(document.getElementById("MAX_slider").value);
+}
+
+function Dec_MAXslider(){
+	document.getElementById("MAX_slider").value = parseFloat(document.getElementById("MAX_slider").value) - 0.1;
+	document.getElementById("originMAX").value = parseFloat(document.getElementById("MAX_slider").value);
+}
